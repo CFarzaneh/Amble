345207e81bffabd0d4220c8c4c5a1a9761b5c994
Cameron <cameron.farzaneh@gmail.com>
2016-03-05 15:21:18 -0500

    Added Twitter authentication

--- Amble/ViewController.swift
+++ Amble/ViewController.swift
@@ -7,15 +7,44 @@
 //
 
 import UIKit
+import TwitterKit
+import Alamofire
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        print(<#T##items: Any...##Any#>)
+        let logInButton = TWTRLogInButton { (session, error) in
+            if let unwrappedSession = session {
+                let alert = UIAlertController(title: "Logged In",
+                    message: "User \(unwrappedSession.userName) has logged in",
+                    preferredStyle: UIAlertControllerStyle.Alert
+                )
+                alert.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.Default, handler: nil))
+                self.presentViewController(alert, animated: true, completion: nil)
+                
+                Alamofire.request(.GET, "http://2cc725cc.ngrok.io/api/dreamText", parameters: ["dream": "\(unwrappedSession.userName)"])
+                    .responseJSON { response in
+                        
+                        print(response.request!)  // original URL request
+                        print(response.response!) // URL response
+                        print(response.data!)     // server data
+                        print(response)   // result of response serialization
+                
+                }
+                
+                
+            } else {
+                NSLog("Login error: %@", error!.localizedDescription);
+            }
+        }
+        
+        // TODO: Change where the log in button is positioned in your view
+        logInButton.center = self.view.center
+        self.view.addSubview(logInButton)
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
